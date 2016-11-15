3e8f9a5ce8630ec72f7dabc330c01cfa0e2e2660
Kerndog73 <kerndog73@gmail.com>
2016-11-15 18:36:13 +1030

    Added alignTo and alignFor

--- Game Engine/Engine/Memory/byteptr.hpp
+++ Game Engine/Engine/Memory/byteptr.hpp
@@ -17,11 +17,21 @@
 using Byte = uint8_t;
 
 namespace Memory {
-  inline uintptr_t toNum(Byte *bytePtr) {
+  inline uintptr_t toNum(void *bytePtr) {
     return reinterpret_cast<uintptr_t>(bytePtr);
   }
   inline Byte *toByte(void *ptr) {
     return reinterpret_cast<Byte *>(ptr);
+  }
+  inline void *alignTo(void *ptr, size_t align) {
+    uintptr_t outOfAlign = toNum(ptr) % align;
+    // x % x == 0 so without the check we would waste memory when the ptr
+    // is aligned
+    return outOfAlign ? toByte(ptr) + (align - outOfAlign) : ptr;
+  }
+  template <typename T>
+  inline void *alignFor(void *ptr) {
+    return alignTo(ptr, alignof(T));
   }
   template <typename T>
   T& deref(Byte *bytePtr) {
