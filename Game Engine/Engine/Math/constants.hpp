cf338b63a0d98fe41b7685499a1a7d4e10d78116
Kerndog73 <kerndog73@gmail.com>
2016-11-16 14:18:26 +1030

    Added more multiples of pi

--- Game Engine/Engine/Math/constants.hpp
+++ Game Engine/Engine/Math/constants.hpp
@@ -14,8 +14,16 @@ namespace Math {
   constexpr double PI    = 3.1415926535897932;
   ///PI * 2
   constexpr double PIt2  = 6.2831853071795864;
+  ///PI * 3
+  constexpr double PIt3  = 9.4247779607693797;
+  ///PI * 4
+  constexpr double PIt4  = 12.566370614359173;
   ///PI / 2
   constexpr double PId2  = 1.5707963267948966;
+  ///PI / 3
+  constexpr double PId3  = 1.04719755119659775;
+  ///PI / 4
+  constexpr double PId4  = 0.78539816339744831;
   ///The base of a natural logarithm
   constexpr double E     = 2.7182818284590452;
   ///The Golden Ratio
