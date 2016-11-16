b5609c2c65d9d2b8e09363c73fb619e25fe01d29
Kerndog73 <kerndog73@gmail.com>
2016-11-16 15:28:25 +1030

    Added debugmacros.hpp

---
+++ Game Engine/Engine/debugmacros.hpp
@@ -0,0 +1,18 @@
+//
+//  debugmacros.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 16/11/16.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#ifndef debugmacros_hpp
+#define debugmacros_hpp
+
+#ifdef NDEBUG
+#define RELEASE
+#else
+#define DEBUG
+#endif
+
+#endif
