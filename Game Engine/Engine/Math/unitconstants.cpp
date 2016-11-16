9a20a34aebd23b49bdf0f7321eba7ea657db59e9
Kerndog73 <kerndog73@gmail.com>
2016-11-16 13:53:52 +1030

    Added imperial/metric conversions

---
+++ Game Engine/Engine/Math/unitconstants.cpp
@@ -0,0 +1,9 @@
+//
+//  unitconstants.cpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 16/11/16.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#include "unitconstants.hpp"
---
+++ Game Engine/Engine/Math/unitconstants.hpp
@@ -0,0 +1,39 @@
+//
+//  unitconstants.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 16/11/16.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#ifndef unitconstants_hpp
+#define unitconstants_hpp
+
+namespace Math {
+  namespace Unit {
+    namespace Length {
+      constexpr double METER_INCH = 39.3700787401575;
+      constexpr double METER_FOOT = 3.28083989501312;
+      constexpr double METER_YARD = 1.09361329833771;
+      constexpr double METER_MILE = 0.000621371192237334;
+      constexpr double INCH_METER = 0.0254;
+      constexpr double INCH_FOOT  = 0.0833333333333333;
+      constexpr double INCH_YARD  = 0.0277777777777778;
+      constexpr double INCH_MILE  = 0.0000157828282828283;
+      constexpr double FOOT_METER = 0.3048;
+      constexpr double FOOT_INCH  = 12;
+      constexpr double FOOT_YARD  = 0.333333333333333;
+      constexpr double FOOT_MILE  = 0.000189393939393939;
+      constexpr double YARD_METER = 0.9144;
+      constexpr double YARD_INCH  = 36;
+      constexpr double YARD_FOOT  = 3;
+      constexpr double YARD_MILE  = 0.000568181818181818;
+      constexpr double MILE_METER = 1609.344;
+      constexpr double MILE_INCH  = 63360;
+      constexpr double MILE_FOOT  = 5280;
+      constexpr double MILE_YARD  = 1760;
+    }
+  }
+}
+
+#endif
