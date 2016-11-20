8c2f5af0d3f0ccadbceb82f205ed774bdb2cfc90
Kerndog73 <kerndog73@gmail.com>
2016-11-20 16:53:25 +1030

    Made use of std::enable_if

--- Game Engine/Engine/Memory/bits.hpp
+++ Game Engine/Engine/Memory/bits.hpp
@@ -13,35 +13,36 @@
 #include <cassert>
 #include <type_traits>
 
+#define MUST_BE_INTEGRAL(RET) typename std::enable_if<std::is_integral<T>::value, RET>::type
+
 namespace Memory {
   namespace Bit {
     template <typename T>
-    constexpr T toMask(uint8_t i) {
-      static_assert(std::is_integral<T>::value, "Bitwise operations can only be performed on integral types");
+    constexpr MUST_BE_INTEGRAL(T) toMask(uint8_t i) {
       return (static_cast<T>(1) << i);
     }
     template <typename T>
-    inline T flip(T num, uint8_t i) {
+    inline MUST_BE_INTEGRAL(T) flip(T num, uint8_t i) {
       assert(i < 64);
       return num ^ toMask<T>(i);
     }
     template <typename T>
-    inline T on(T num, uint8_t i) {
+    inline MUST_BE_INTEGRAL(T) on(T num, uint8_t i) {
       assert(i < 64);
       return num | toMask<T>(i);
     }
     template <typename T>
-    inline T off(T num, uint8_t i) {
+    inline MUST_BE_INTEGRAL(T) off(T num, uint8_t i) {
       assert(i < 64);
       return num & ~toMask<T>(i);
     }
     template <typename T>
-    inline T set(T num, uint8_t i, bool value) {
+    inline MUST_BE_INTEGRAL(T) set(T num, uint8_t i, bool value) {
       assert(i < 64);
       return value ? num | toMask<T>(i) : num & ~toMask<T>(i);
     }
     template <typename T>
-    inline bool get(T num, uint8_t i) {
+    inline MUST_BE_INTEGRAL(bool) get(T num, uint8_t i) {
       assert(i < 64);
       return num & toMask<T>(i);
     }
