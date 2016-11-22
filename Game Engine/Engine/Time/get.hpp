36ca46b3f1583fc6e7bc981b175c3b9238ab0611
Kerndog73 <kerndog73@gmail.com>
2016-11-22 14:20:51 +1030

    Added Unit enum

--- Game Engine/Engine/Time/get.hpp
+++ Game Engine/Engine/Time/get.hpp
@@ -10,9 +10,16 @@
 #define get_hpp
 
 #include <chrono>
-#include "Math/siconstants.hpp"
+#include "../Math/siconstants.hpp"
 
 namespace Time {
+  enum Unit {
+    NANO,
+    MICRO,
+    MILLI,
+    SEC
+  };
+
   ///Get current time in nanoseconds as an integer
   inline uint64_t getNanoI() {
     return std::chrono::high_resolution_clock::now().time_since_epoch().count();
@@ -30,6 +37,19 @@ namespace Time {
     return getNanoI() * Math::SI::NANO_ONE;
   }
   
+  inline uint64_t getI(Unit unit) {
+    switch (unit) {
+      case NANO:
+        return getNanoI();
+      case MICRO:
+        return getMicroI();
+      case MILLI:
+        return getMilliI();
+      case SEC:
+        return getSecI();
+    }
+  }
+  
   ///Get current time in nanoseconds as floating point number
   inline double getNanoF() {
     return std::chrono::high_resolution_clock::now().time_since_epoch().count();
@@ -45,6 +65,19 @@ namespace Time {
   ///Get current time in seconds as floating point number
   inline double getSecF() {
     return getNanoF() * Math::SI::NANO_ONE;
+  }
+  
+  inline double getF(Unit unit) {
+    switch (unit) {
+      case NANO:
+        return getNanoF();
+      case MICRO:
+        return getMicroF();
+      case MILLI:
+        return getMilliF();
+      case SEC:
+        return getSecF();
+    }
   }
   
   ///Get the Unix timestamp
