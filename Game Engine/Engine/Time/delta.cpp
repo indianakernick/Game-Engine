76288ee80f5cc0ea63798030b522789eb1273745
Kerndog73 <kerndog73@gmail.com>
2016-11-27 18:00:34 +1030

    Removed unnecessary files

--- Game Engine/Engine/Time/delta.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  delta.cpp
-//  Game Engine
-//
-//  Created by Indi Kernick on 22/11/16.
-//  Copyright © 2016 Indi Kernick. All rights reserved.
-//
-
-#include "delta.hpp"
--- Game Engine/Engine/Time/interval.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  interval.cpp
-//  Game Engine
-//
-//  Created by Indi Kernick on 26/11/16.
-//  Copyright © 2016 Indi Kernick. All rights reserved.
-//
-
-#include "interval.hpp"
