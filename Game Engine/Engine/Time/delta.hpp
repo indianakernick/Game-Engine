69d0e88a6689eda6a81f4daa31db6fb0ccc6b685
Kerndog73 <kerndog73@gmail.com>
2016-11-28 16:42:00 +1030

    Removed getDuration

--- Game Engine/Engine/Time/delta.hpp
+++ Game Engine/Engine/Time/delta.hpp
@@ -20,15 +20,10 @@ namespace Time {
     
     ///Get the duration that has passed since the last call of this function
     uint64_t get() {
-      return getDuration().count();
-    }
-    
-    ///Get the duration that has passed since the last call of this function
-    DURATION_TYPE getDuration() {
       Point<DURATION_TYPE> now = getPoint<DURATION_TYPE>();
       DURATION_TYPE delta = now - lastTime;
       lastTime = now;
-      return delta;
+      return delta.count();
     }
   private:
     Point<DURATION_TYPE> lastTime;
