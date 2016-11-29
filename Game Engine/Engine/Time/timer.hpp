137a82636e942fd54eb917b564fa91b4272d2940
Kerndog73 <kerndog73@gmail.com>
2016-11-29 16:53:05 +1030

    Renamed "timer" to "stopwatch" and added new "timer"

--- Game Engine/Engine/Time/timer.hpp
+++ Game Engine/Engine/Time/timer.hpp
@@ -2,7 +2,7 @@
 //  timer.hpp
 //  Game Engine
 //
-//  Created by Indi Kernick on 28/11/16.
+//  Created by Indi Kernick on 29/11/16.
 //  Copyright © 2016 Indi Kernick. All rights reserved.
 //
 
@@ -13,49 +13,89 @@
 #include <cassert>
 
 namespace Time {
+  ///A timer to count down for duration
   template <typename DURATION_TYPE>
   class Timer {
+  private:
+    enum State : char {
+      INITIAL,
+      RUNNING,
+      PAUSED,
+      DONE
+    };
   public:
     Timer() = default;
     
-    ///Starts the timer
+    ///Sets the duration to be counted down from when the timer starts
+    void setDuration(uint64_t newDuration) {
+      assert(state == INITIAL || state == DONE);
+      duration = newDuration;
+    }
+    ///Starts the timer with the set duration
     void start() {
-      assert(!running);
-      running = true;
-      startTime = getPoint<DURATION_TYPE>();
+      assert(state == INITIAL || state == DONE);
+      state = RUNNING;
+      startPoint = getI<DURATION_TYPE>();
     }
-    ///Stops the timer and returns the duration
-    uint64_t stop() {
-      assert(running);
-      endTime = getPoint<DURATION_TYPE>();
-      running = false;
-      return (endTime - startTime).count();
+    ///Starts the timer with the duration given
+    void start(uint64_t newDuration) {
+      assert(state == INITIAL || state == DONE);
+      state = RUNNING;
+      duration = newDuration;
+      startPoint = getI<DURATION_TYPE>();
     }
-    ///Resume the timer after it has been stopped, returning the duration
+    ///Pause the timer and returns the time left
+    uint64_t pause() {
+      assert(state == RUNNING);
+      state = PAUSED;
+      uint64_t now = getI<DURATION_TYPE>();
+      pausePoint = now;
+      return duration - (now - startPoint);
+    }
+    ///Resume the timer and returns the time left
     uint64_t resume() {
-      assert(!running);
-      running = true;
-      return (endTime - startTime).count();
+      assert(state == PAUSED);
+      state = RUNNING;
+      uint64_t now = getI<DURATION_TYPE>();
+      //move the startPoint forward so that timeLeft is the same as it was
+      //when it was paused
+      startPoint += now - pausePoint;
+      return duration - (now - startPoint);
     }
-    ///Gets the duration. Can be called while the timer is running
+    ///Get the amount of time left before the timer is done
     uint64_t get() {
-      return (endTime - startTime).count();
+      assert(state != INITIAL);
+      
+      if (state == INITIAL || state == DONE) {
+        return 0;
+      }
+      
+      uint64_t now = getI<DURATION_TYPE>();
+      
+      if (state == PAUSED) {
+        startPoint += now - pausePoint;
+        pausePoint = now;
+      }
+      
+      uint64_t timeLeft = duration - (now - startPoint);
+      //if timeLeft overflows then we've passed the duration
+      if (timeLeft > duration) {
+        state = DONE;
+        return 0;
+      } else {
+        return timeLeft;
+      }
     }
-    ///Returns the duration and resets the timer
-    uint64_t lap() {
-      assert(running);
-      uint64_t duration = (endTime - startTime).count();
-      startTime = getPoint<DURATION_TYPE>();
-      return duration;
-    }
-    ///Returns whether the timer is running (start has just been called).
-    bool isRunning() {
-      return running;
+    ///Checks whether the timer is done
+    bool done() {
+      get();
+      return state == DONE;
     }
   private:
-    Point<DURATION_TYPE> startTime = 0;
-    Point<DURATION_TYPE> endTime = 0;
-    bool running = false;
+    uint64_t duration = 0;
+    uint64_t startPoint = 0;
+    uint64_t pausePoint = 0;
+    State state = INITIAL;
   };
 }
 
