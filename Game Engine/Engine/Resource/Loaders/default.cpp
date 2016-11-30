7480baa7deb7fb17cfbadec23965191df58a574d
Kerndog73 <kerndog73@gmail.com>
2016-11-30 16:56:05 +1030

    Moved default loader into loaders folder

---
+++ Game Engine/Engine/Resource/Loaders/default.cpp
@@ -0,0 +1,23 @@
+//
+//  default.cpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 19/11/16.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#include "default.hpp"
+
+bool Resource::Loaders::Default::canLoad(const std::string &) {
+  return true;
+}
+
+size_t Resource::Loaders::Default::getSize(const Memory::Buffer file) {
+  return file.size();
+}
+
+bool Resource::Loaders::Default::useRaw() {
+  return true;
+}
+
+void Resource::Loaders::Default::process(const Memory::Buffer, Memory::Buffer) {}
