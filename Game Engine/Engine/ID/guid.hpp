baadbcf8098a09e5a1bd9ff8aebd32cebb374567
Kerndog73 <kerndog73@gmail.com>
2016-12-15 09:36:35 +1030

    Added setRange function

--- Game Engine/Engine/ID/guid.hpp
+++ Game Engine/Engine/ID/guid.hpp
@@ -16,17 +16,25 @@ namespace ID {
   //the set id allows you to group sets of GUIDs together
   template <typename T, int SET_ID = 0>
   class GUID {
+  private:
+    template <typename RET>
+    using IsArithmetic = typename std::enable_if<std::is_arithmetic<T>::value, RET>::type;
   public:
     GUID() = delete;
     ~GUID() = delete;
     
-    static auto get() -> typename std::enable_if<std::is_arithmetic<T>::value, T>::type {
+    static auto get() -> IsArithmetic<T> {
       T newID;
       do {
         newID = dist(gen);
       } while (used.count(newID));
       used.insert(newID);
       return newID;
+    }
+    
+    static auto setRange(T min, T max) -> IsArithmetic<void> {
+      assert(min < max);
+      dist = {min, max};
     }
   private:
     static std::unordered_set<T> used;
