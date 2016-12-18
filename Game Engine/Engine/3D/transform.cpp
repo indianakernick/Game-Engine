598ba0d3a12e158f03dc84ebbe84cfeb85d97b7f
Kerndog73 <kerndog73@gmail.com>
2016-12-18 16:18:14 +1030

    Encapsulated data members and cached matrices

--- Game Engine/Engine/3D/transform.cpp
+++ Game Engine/Engine/3D/transform.cpp
@@ -1,5 +1,5 @@
 //
-//  coord.cpp
+//  transform.cpp
 //  Game Engine
 //
 //  Created by Indi Kernick on 7/09/2016.
@@ -8,9 +8,88 @@
 
 #include "transform.hpp"
 
-glm::mat4 Transform::getMat() const {
-  //rst rotation scale translation
-  glm::mat4 mat = glm::mat4_cast(rot);
-  glm::scale(mat, scale);
-  return glm::translate(mat, pos);
+Graphics3D::Transform::Transform(const glm::mat4 &mat) {
+  setMat(mat);
+}
+
+void Graphics3D::Transform::setMat(const glm::mat4 &newMat) {
+  mat = newMat;
+  invMat = glm::inverse(newMat);
+  glm::vec3 skew;
+  glm::vec4 perspective;
+  glm::decompose(newMat, scale, rot, pos, skew, perspective);
+  matChanged = false;
+}
+
+const glm::mat4 &Graphics3D::Transform::getMat() const {
+  calcMat();
+  return mat;
+}
+
+const glm::mat4 &Graphics3D::Transform::getInvMat() const {
+  calcMat();
+  return invMat;
+}
+
+void Graphics3D::Transform::moveTo(const glm::vec3 &newPos) {
+  pos = newPos;
+  matChanged = true;
+}
+
+void Graphics3D::Transform::moveBy(const glm::vec3 &delta) {
+  pos += delta;
+  matChanged = true;
+}
+
+const glm::vec3 &Graphics3D::Transform::getPos() const {
+  return pos;
+}
+
+void Graphics3D::Transform::scaleTo(const glm::vec3 &newScale) {
+  scale = newScale;
+  matChanged = true;
+}
+
+void Graphics3D::Transform::scaleBy(const glm::vec3 &multiple) {
+  scale *= multiple;
+  matChanged = true;
+}
+
+const glm::vec3 &Graphics3D::Transform::getScale() const {
+  return scale;
+}
+
+void Graphics3D::Transform::rotateTo(const glm::quat &newRot) {
+  rot = newRot;
+  matChanged = true;
+}
+
+void Graphics3D::Transform::rotateTo(float angle, const glm::vec3 &axis) {
+  glm::quat identity;
+  rot = glm::rotate(identity, angle, axis);
+  matChanged = true;
+}
+
+void Graphics3D::Transform::rotateBy(const glm::quat &delta) {
+  //delta doesn't seem like the right name but it sort of makes sense...
+  rot *= delta;
+  matChanged = true;
+}
+
+void Graphics3D::Transform::rotateBy(float angle, const glm::vec3 &axis) {
+  rot = glm::rotate(rot, angle, axis);
+  matChanged = true;
+}
+
+const glm::quat &Graphics3D::Transform::getRotation() const {
+  return rot;
+}
+
+void Graphics3D::Transform::calcMat() const {
+  if (matChanged) {
+    //RST rotation scale translation
+    mat = glm::translate(glm::scale(glm::mat4_cast(rot), scale), pos);
+    invMat = glm::inverse(mat);
+    matChanged = false;
+  }
 }
--- Game Engine/Engine/3D/transform.hpp
+++ Game Engine/Engine/3D/transform.hpp
@@ -1,26 +1,62 @@
 //
-//  transform3d.hpp
+//  transform.hpp
 //  Game Engine
 //
 //  Created by Indi Kernick on 7/09/2016.
 //  Copyright © 2016 Indi Kernick. All rights reserved.
 //
 
-#ifndef engine_3d_transform3d_hpp
-#define engine_3d_transform3d_hpp
+#ifndef engine_3d_transform_hpp
+#define engine_3d_transform_hpp
 
-#include <glm/gtc/quaternion.hpp>
+#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 
-class Transform {
-public:
-  Transform() = default;
-  
-  glm::mat4 getMat() const;
-  
-  glm::vec3 pos;
-  glm::vec3 scale {1,1,1};
-  glm::quat rot;
-};
+namespace Graphics3D {
+  class Transform {
+  public:
+    Transform() = default;
+    explicit Transform(const glm::mat4 &);
+    
+    void setMat(const glm::mat4 &);
+    const glm::mat4 &getMat() const;
+    const glm::mat4 &getInvMat() const;
+    
+    //sets the position
+    void moveTo(const glm::vec3 &);
+    //adds the position
+    void moveBy(const glm::vec3 &);
+    const glm::vec3 &getPos() const;
+    
+    //sets the scale
+    void scaleTo(const glm::vec3 &);
+    //multiplies the scale
+    void scaleBy(const glm::vec3 &);
+    const glm::vec3 &getScale() const;
+    
+    //sets the rotation
+    void rotateTo(const glm::quat &);
+    void rotateTo(float, const glm::vec3 &);
+    //adds the rotation
+    void rotateBy(const glm::quat &);
+    void rotateBy(float, const glm::vec3 &);
+    const glm::quat &getRotation() const;
+    
+  private:
+    glm::vec3 pos;
+    glm::vec3 scale {1, 1, 1};
+    glm::quat rot;
+    
+    //they're mutable because they are potentially modified in getMat
+    //and getInvMat
+    mutable glm::mat4 mat;
+    mutable glm::mat4 invMat;
+    //set to true when the matrix needs to be recalculated
+    mutable bool matChanged = false;
+    
+    void calcMat() const;
+  };
+}
 
 #endif
