40577636a57ddf4db17a0e35ab2dfe63430ed40e
Kerndog73 <kerndog73@gmail.com>
2016-12-28 14:57:38 +1030

    Added header for OpenGL

---
+++ Game Engine/Engine/Application/opengl.hpp
@@ -0,0 +1,16 @@
+//
+//  opengl.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 28/12/16.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#ifndef engine_application_opengl_hpp
+#define engine_application_opengl_hpp
+
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+#endif
