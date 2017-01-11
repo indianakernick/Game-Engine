fb4d813403d01936fa50786e052425d875e798f2
Kerndog73 <kerndog73@gmail.com>
2017-01-11 17:44:35 +1030

    Deleted empty file

--- Game Engine/Engine/Resource/shorter namespace.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  shorter namespace.cpp
-//  Game Engine
-//
-//  Created by Indi Kernick on 11/1/17.
-//  Copyright © 2017 Indi Kernick. All rights reserved.
-//
-
-#include "shorter namespace.hpp"
