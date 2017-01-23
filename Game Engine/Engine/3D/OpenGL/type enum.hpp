03b00b9292cd11dd3aef18a3c43f84e3e172c3d0
Kerndog73 <kerndog73@gmail.com>
2017-01-24 10:28:20 +1030

    Added vector types

--- Game Engine/Engine/3D/OpenGL/type enum.hpp
+++ Game Engine/Engine/3D/OpenGL/type enum.hpp
@@ -17,51 +17,46 @@ namespace Graphics3D {
   template <typename T>
   struct TypeEnum {};
   
-  ///GLboolean is defined as a uint8_t so the types conflict
-  template <>
-  struct TypeEnum<bool> {
-    static const GLenum value = GL_BOOL;
+  #define TYPE_ENUM(type, enum) \
+  template <>\
+  struct TypeEnum<type> {\
+    static const GLenum value = enum;\
   };
   
-  template <>
-  struct TypeEnum<GLbyte> {
-    static const GLenum value = GL_BYTE;
-  };
+  //GLboolean is defined as a uint8_t so the types conflict
   
-  template <>
-  struct TypeEnum<GLubyte> {
-    static const GLenum value = GL_UNSIGNED_BYTE;
-  };
+  TYPE_ENUM(bool, GL_BOOL)
+  TYPE_ENUM(GLbyte, GL_BYTE)
+  TYPE_ENUM(GLubyte, GL_UNSIGNED_BYTE)
+  TYPE_ENUM(GLshort, GL_SHORT)
+  TYPE_ENUM(GLushort, GL_UNSIGNED_SHORT)
+  TYPE_ENUM(GLint, GL_INT)
+  TYPE_ENUM(GLuint, GL_UNSIGNED_INT)
+  TYPE_ENUM(GLfloat, GL_FLOAT)
+  TYPE_ENUM(GLdouble, GL_DOUBLE)
   
-  template <>
-  struct TypeEnum<GLshort> {
-    static const GLenum value = GL_SHORT;
-  };
+  TYPE_ENUM(bool[2], GL_BOOL_VEC2)
+  TYPE_ENUM(GLbyte[2], GL_2_BYTES)
+  TYPE_ENUM(GLint[2], GL_INT_VEC2)
+  TYPE_ENUM(GLuint[2], GL_UNSIGNED_INT_VEC2)
+  TYPE_ENUM(GLfloat[2], GL_FLOAT_VEC2)
+  TYPE_ENUM(GLdouble[2], GL_DOUBLE_VEC2)
   
-  template <>
-  struct TypeEnum<GLushort> {
-    static const GLenum value = GL_UNSIGNED_SHORT;
-  };
+  TYPE_ENUM(bool[3], GL_BOOL_VEC3)
+  TYPE_ENUM(GLbyte[3], GL_3_BYTES)
+  TYPE_ENUM(GLint[3], GL_INT_VEC3)
+  TYPE_ENUM(GLuint[3], GL_UNSIGNED_INT_VEC3)
+  TYPE_ENUM(GLfloat[3], GL_FLOAT_VEC3)
+  TYPE_ENUM(GLdouble[3], GL_DOUBLE_VEC3)
   
-  template <>
-  struct TypeEnum<GLint> {
-    static const GLenum value = GL_INT;
-  };
+  TYPE_ENUM(bool[4], GL_BOOL_VEC4)
+  TYPE_ENUM(GLbyte[4], GL_4_BYTES)
+  TYPE_ENUM(GLint[4], GL_INT_VEC4)
+  TYPE_ENUM(GLuint[4], GL_UNSIGNED_INT_VEC4)
+  TYPE_ENUM(GLfloat[4], GL_FLOAT_VEC4)
+  TYPE_ENUM(GLdouble[4], GL_DOUBLE_VEC4)
   
-  template <>
-  struct TypeEnum<GLuint> {
-    static const GLenum value = GL_UNSIGNED_INT;
-  };
-  
-  template <>
-  struct TypeEnum<GLfloat> {
-    static const GLenum value = GL_FLOAT;
-  };
-  
-  template <>
-  struct TypeEnum<GLdouble> {
-    static const GLenum value = GL_DOUBLE;
-  };
+  #undef TYPE_ENUM
 }
 
 #endif
