905f5bc152cd4390d3e5b690be4283c737c0ccd1
Kerndog73 <kerndog73@gmail.com>
2017-02-12 12:05:25 +1030

    Added error checks

--- Game Engine/Engine/Resource/loader.cpp
+++ Game Engine/Engine/Resource/loader.cpp
@@ -13,22 +13,35 @@ std::string Res::Loader::absPath(const ID &id) {
 }
 
 std::ifstream Res::Loader::openFileStream(const ID &id) {
-  return std::ifstream(absPath(id));
+  std::ifstream stream(absPath(id), std::ios::binary | std::ios::in);
+  if (!stream.is_open()) {
+    LOG_ERROR(RESOURCES, "Failed to open file stream \"%s\"", id.getPathC());
+  }
+  return stream;
 }
 
 std::FILE *Res::Loader::openFile(const ID &id) {
-  return std::fopen(absPath(id).c_str(), "rb");
+  std::FILE *file = std::fopen(absPath(id).c_str(), "rb");
+  if (file == nullptr) {
+    LOG_ERROR(RESOURCES, "Failed to open file \"%s\"", id.getPathC());
+  }
+  return file;
 }
 
 std::pair<Memory::Buffer, bool> Res::Loader::readFile(const ID &id) {
-  std::FILE *file = std::fopen(absPath(id).c_str(), "rb");
+  std::FILE *file = openFile(id);
   if (file) {
     std::fseek(file, 0, SEEK_END);
     Memory::Buffer buffer(std::ftell(file));
     std::rewind(file);
-    std::fread(buffer.begin(), buffer.size(), 1, file);
-    std::fclose(file);
-    return {buffer, true};
+    if (std::fread(buffer.begin(), buffer.size(), 1, file)) {
+      std::fclose(file);
+      return {buffer, true};
+    } else {
+      LOG_ERROR(RESOURCES, "Failed to read file \"%s\"", id.getPathC());
+      std::fclose(file);
+      return {Memory::Buffer(1), false};
+    }
   } else {
     return {Memory::Buffer(1), false};
   }
