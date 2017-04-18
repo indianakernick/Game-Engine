b4263e25ea84f2ef35a0d1a8e39e4f823eddef2b
Kerndog73 <kerndog73@gmail.com>
2017-04-18 14:43:49 +0930

    Replaced InstanceCounter with LimitInstances

---
+++ Game Engine/Engine/Utils/instance limiter.hpp
@@ -0,0 +1,148 @@
+//
+//  instance limiter.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 8/10/2016.
+//  Copyright © 2016 Indi Kernick. All rights reserved.
+//
+
+#ifndef engine_utils_instance_limiter_hpp
+#define engine_utils_instance_limiter_hpp
+
+#ifdef ENABLE_INSTANCE_LIMITER
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio>
+#include "type name.hpp"
+
+///Limit the number of instances of a derived class
+template <typename T, size_t MAX_COUNT>
+class LimitInstances {
+private:
+  void checkCount() const {
+    if (count > MAX_COUNT) {
+      std::printf(
+        "Too many instances of type %s\nMaximum is %lu\n",
+        typeName<T>().c_str(),
+        MAX_COUNT
+      );
+      std::abort();
+    }
+  }
+
+protected:
+  LimitInstances() {
+    count++;
+    checkCount();
+  }
+  LimitInstances(const LimitInstances &) {
+    count++;
+    checkCount();
+  }
+  LimitInstances(LimitInstances &&) {
+    count++;
+  }
+  ~LimitInstances() {
+    count--;
+  }
+  
+  LimitInstances &operator=(const LimitInstances &) = default;
+  LimitInstances &operator=(LimitInstances &&) = default;
+
+private:
+  static size_t count;
+};
+
+template <typename T, size_t MAX_COUNT>
+size_t LimitInstances<T, MAX_COUNT>::count = 0;
+
+///Limit the number of instances of a derived class
+template <typename T>
+class LimitInstances<T, 1> {
+private:
+  void checkCount() const {
+    if (count > 1) {
+      std::printf(
+        "Too many instances of singleton type %s\n",
+        typeName<T>().c_str()
+      );
+      std::abort();
+    }
+  }
+
+protected:
+  LimitInstances() {
+    count++;
+    checkCount();
+  }
+  LimitInstances(const LimitInstances &) = delete;
+  LimitInstances(LimitInstances &&) {
+    count++;
+  }
+  ~LimitInstances() {
+    count--;
+  }
+  
+  LimitInstances &operator=(const LimitInstances &) = delete;
+  LimitInstances &operator=(LimitInstances &&) = delete;
+
+private:
+  static size_t count;
+};
+
+template <typename T>
+size_t LimitInstances<T, 1>::count = 0;
+
+#else
+
+#include <cstddef>
+
+///Limit the number of instances of a derived class
+template <typename, size_t>
+class LimitInstances {
+protected:
+  LimitInstances() = default;
+  LimitInstances(const LimitInstances &) = default;
+  LimitInstances(LimitInstances &&) = default;
+  ~LimitInstances() = default;
+  
+  LimitInstances &operator=(const LimitInstances &) = default;
+  LimitInstances &operator=(LimitInstances &&) = default;
+};
+
+///Limit the number of instances of a derived class
+template <typename T>
+class LimitInstances<T, 1> {
+protected:
+  LimitInstances() = default;
+  LimitInstances(const LimitInstances &) = delete;
+  LimitInstances(LimitInstances &&) = default;
+  ~LimitInstances() = default;
+  
+  LimitInstances &operator=(const LimitInstances &) = delete;
+  LimitInstances &operator=(LimitInstances &&) = delete;
+};
+
+#endif
+
+//I don't think there is any point in this
+
+///Limit the number of instances of a derived class
+template <typename T>
+class LimitInstances<T, 0> {
+protected:
+  LimitInstances() = delete;
+  LimitInstances(const LimitInstances &) = delete;
+  LimitInstances(LimitInstances &&) = delete;
+  ~LimitInstances() = delete;
+  
+  LimitInstances &operator=(const LimitInstances &) = delete;
+  LimitInstances &operator=(LimitInstances &&) = delete;
+};
+
+///Force a derived class to be a singleton
+template <typename T>
+using ForceSingleton = LimitInstances<T, 1>;
+
+#endif
---
+++ Game Engine/Engine/Utils/type name.hpp
@@ -0,0 +1,29 @@
+//
+//  type name.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 18/4/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef engine_utils_type_name_hpp
+#define engine_utils_type_name_hpp
+
+#include <string>
+
+///Demangles the name returned by std::type_info::name()
+std::string demangle(const char *);
+
+///Get the formatted name for the type of a value
+template <typename T>
+std::string valueTypeName(const T &v) {
+  return demangle(typeid(v).name());
+}
+
+///Get the formatted name for a type
+template <typename T>
+std::string typeName() {
+  return demangle(typeid(T).name());
+}
+
+#endif
