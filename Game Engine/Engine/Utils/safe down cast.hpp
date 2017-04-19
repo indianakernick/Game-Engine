7888be0f8f5080a4676bf7deb532b294c8cc67ee
Kerndog73 <kerndog73@gmail.com>
2017-04-19 14:40:17 +0930

    Added safeLock

--- Game Engine/Engine/Utils/safe down cast.hpp
+++ Game Engine/Engine/Utils/safe down cast.hpp
@@ -12,6 +12,43 @@
 #include <memory>
 #include <cassert>
 
+#ifdef NDEBUG
+
+///Dynamic cast and assert that it was successful
+template <typename DERIVED, typename BASE>
+inline std::enable_if_t<
+  std::is_base_of<BASE, DERIVED>::value &&
+  !std::is_same<BASE, DERIVED>::value,
+  DERIVED *
+>
+safeDownCast(BASE * const base) {
+  return static_cast<DERIVED *>(base);
+}
+
+///Dynamic cast and assert that it was successful
+template <typename DERIVED, typename BASE>
+inline std::enable_if_t<
+  std::is_base_of<BASE, DERIVED>::value &&
+  !std::is_same<BASE, DERIVED>::value,
+  const DERIVED *
+>
+safeDownCast(const BASE * const base) {
+  return static_cast<const DERIVIED *>(base);
+}
+
+///Dynamic cast and assert that it was successful
+template <typename DERIVED, typename BASE>
+inline std::enable_if_t<
+  std::is_base_of<BASE, DERIVED>::value &&
+  !std::is_same<BASE, DERIVED>::value,
+  std::shared_ptr<DERIVED>
+>
+safeDownCast(const std::shared_ptr<BASE> &base) {
+  return std::static_pointer_cast<DERIVED>(base);
+}
+
+#else
+
 ///Dynamic cast and assert that it was successful
 template <typename DERIVED, typename BASE>
 inline std::enable_if_t<
@@ -21,7 +58,7 @@ inline std::enable_if_t<
 >
 safeDownCast(BASE * const base) {
   DERIVED * const derived = dynamic_cast<DERIVED *>(base);
-  assert(dynamic_cast<DERIVED *>(base));
+  assert(derived);
   return derived;
 }
 
@@ -49,6 +86,16 @@ safeDownCast(const std::shared_ptr<BASE> &base) {
   const std::shared_ptr<DERIVED> derived = std::dynamic_pointer_cast<DERIVED>(base);
   assert(derived);
   return derived;
+}
+
+#endif
+
+///Lock a std::weak_ptr and assert that it was successful
+template <typename T>
+inline std::shared_ptr<T> safeLock(const std::weak_ptr<T> &weak) {
+  const std::shared_ptr<T> strong = weak.lock();
+  assert(strong);
+  return strong;
 }
 
 #endif
