3186e09857b60c9030da4e0e0705beae050bb6cb
Kerndog73 <kerndog73@gmail.com>
2017-06-02 18:29:52 +0930

    Add GroupDispatcher

--- Game Engine/Engine/Utils/dispatcher.hpp
+++ Game Engine/Engine/Utils/dispatcher.hpp
@@ -25,7 +25,7 @@ struct RetHandler {
 }
 */
 
-template <typename Listener, typename RetHandler, typename ListenerId = uint32_t>
+template <typename Listener, typename RetHandler, typename ListenerID = uint32_t>
 class Dispatcher;
 
 template <typename ListenerId, typename RetHandler, typename ListenerRet, typename ...ListenerArgs>
@@ -36,8 +36,8 @@ public:
   
   class BadListenerID final : public std::runtime_error {
   public:
-    explicit BadListenerID(ListenerID id)
-      : std::runtime_error("Dispatcher::remListener was called with a bad ID"), id(id) {}
+    explicit BadListenerID(const ListenerID id)
+      : std::runtime_error("Dispatcher::remListener was called with a bad ListenerID"), id(id) {}
     
     ListenerID getID() const {
       return id;
@@ -72,7 +72,7 @@ public:
     return id;
   }
   
-  void remListener(ListenerID id) {
+  void remListener(const ListenerID id) {
     if (id >= listeners.size()) {
       throw BadListenerID(id);
     }
@@ -84,7 +84,7 @@ public:
     }
   }
 
-  ListenerRet dispatch(ListenerArgs... args) {
+  ListenerRet dispatch(const ListenerArgs... args) {
     if (dispatching) {
       throw BadDispatchCall();
     }
@@ -156,5 +156,191 @@ private:
 ///An action that can be confirmed to happen
 template <typename ListenerID, typename ...Args>
 using Confirmable = Dispatcher<bool (Args...), ConfirmableRetHandler, ListenerID>;
+
+///Divides listeners into groups. A message can be dispatched to each group individually.
+///Many Dispatchers will perform better than one GroupDispatcher
+template <typename Listener, typename RetHandler, typename GroupID = uint32_t, typename ListenerID = uint32_t>
+class GroupDispatcher;
+
+template <typename GroupId, typename ListenerId, typename RetHandler, typename ListenerRet, typename ...ListenerArgs>
+class GroupDispatcher<ListenerRet(ListenerArgs...), RetHandler, GroupId, ListenerId> {
+public:
+  static_assert(std::is_integral<ListenerId>::value);
+  static_assert(std::is_integral<GroupId>::value);
+
+  using Listener = std::function<ListenerRet (ListenerArgs...)>;
+  using ListenerID = ListenerId;
+  using GroupID = GroupId;
+  
+  //used for calculating IDs
+  static constexpr ListenerID MAX_LISTENERS_PER_GROUP = 1 << (sizeof(ListenerID) * 4);
+  static constexpr GroupID MAX_GROUPS = std::min(
+    MAX_LISTENERS_PER_GROUP,
+    std::numeric_limits<GroupID>::max()
+  );
+  
+  class BadListenerID final : public std::runtime_error {
+  public:
+    explicit BadListenerID(const ListenerID id)
+      : std::runtime_error("GroupDispatcher::remListener was called with a bad ListenerID"), id(id) {}
+    
+    ListenerID getID() const {
+      return id;
+    }
+    
+  private:
+    ListenerID id;
+  };
+  
+  class BadGroupID final : public std::runtime_error {
+  public:
+    explicit BadGroupID(const GroupID id)
+      : std::runtime_error("GroupDispatcher::dispatch was called with a bad GroupID"), id(id) {}
+    
+    GroupID getID() const {
+      return id;
+    }
+    
+  private:
+    GroupID id;
+  };
+  
+  class BadListener final : public std::runtime_error {
+  public:
+    BadListener()
+      : std::runtime_error("GroupDispatcher::addListener was called with a null listener") {}
+  };
+  
+  class BadDispatchCall final : public std::logic_error {
+  public:
+    BadDispatchCall()
+      : std::logic_error("GroupDispatcher::dispatch was called from an listener") {}
+  };
+
+  GroupDispatcher() = default;
+  ~GroupDispatcher() = default;
+  
+  ListenerID addListener(const GroupID groupID, const Listener &listener) {
+    if (listener == nullptr) {
+      throw BadListener();
+    }
+    
+    const size_t group = static_cast<size_t>(groupID);
+    while (groups.size() < group) {
+      groups.emplace_back();
+    }
+    const ListenerID listenerID = static_cast<ListenerID>(
+      group * MAX_LISTENERS_PER_GROUP + groups[group].size()
+    );
+    groups[group].emplace_back(listener);
+    return listenerID;
+  }
+  
+  void remListener(ListenerID listenerID) {
+    const size_t group = listenerID / MAX_LISTENERS_PER_GROUP;
+    const size_t listener = listenerID % MAX_LISTENERS_PER_GROUP;
+    
+    if (group >= groups.size() || listener >= groups[group].size()) {
+      throw BadListenerID(listenerID);
+    }
+    
+    if (dispatching) {
+      oldListeners.push_back(listenerID);
+    } else {
+      groups[group][listener] = nullListener;
+    }
+  }
+
+  ListenerRet dispatch(const GroupID groupID, const ListenerArgs... args) {
+    if (dispatching) {
+      throw BadDispatchCall();
+    }
+    
+    dispatching = true;
+    
+    const size_t group = static_cast<GroupID>(groupID);
+    if (group >= groups.size()) {
+      throw BadGroupID(groupID);
+    }
+    const Listeners &listeners = groups[group];
+    
+    if constexpr (std::is_void<ListenerRet>::value) {
+      const auto end = listeners.cend();
+      for (auto l = listeners.cbegin(); l != end; ++l) {
+        (*l)(args...);
+      }
+      dispatching = false;
+      remOldListeners();
+    } else {
+      RetHandler retHandler;
+      const auto end = listeners.cend();
+      for (auto l = listeners.cbegin(); l != end; ++l) {
+        retHandler.handleReturnValue((*l)(args...));
+      }
+      dispatching = false;
+      remOldListeners();
+      
+      return retHandler.getFinalReturnValue();
+    }
+  }
+  
+  ListenerRet dispatchToAll(const ListenerArgs... args) {
+    if (dispatching) {
+      throw BadDispatchCall();
+    }
+    
+    dispatching = true;
+    
+    if constexpr (std::is_void<ListenerRet>::value) {
+      for (auto g = groups.cbegin(); g != groups.cend(); ++g) {
+        const Listeners &listeners = *g;
+        const auto end = listeners.cend();
+        for (auto l = listeners.cbegin(); l != end; ++l) {
+          (*l)(args...);
+        }
+      }
+      dispatching = false;
+      remOldListeners();
+    } else {
+      RetHandler retHandler;
+      for (auto g = groups.cbegin(); g != groups.cend(); ++g) {
+        const Listeners &listeners = *g;
+        const auto end = listeners.cend();
+        for (auto l = listeners.cbegin(); l != end; ++l) {
+          retHandler.handleReturnValue((*l)(args...));
+        }
+      }
+      dispatching = false;
+      remOldListeners();
+      
+      return retHandler.getFinalReturnValue();
+    }
+  }
+  
+private:
+  using Listeners = std::vector<Listener>;
+  using Groups = std::vector<Listeners>;
+  Groups groups;
+  //listeners that will be removed after dispatch finishes
+  std::vector<ListenerID> oldListeners;
+  //dispatch is currently running
+  bool dispatching = false;
+  
+  void remOldListeners() {
+    for (auto l = oldListeners.cbegin(); l != oldListeners.cend(); ++l) {
+      const size_t listenerID = *l;
+      const size_t group = listenerID / MAX_LISTENERS_PER_GROUP;
+      const size_t listener = listenerID % MAX_LISTENERS_PER_GROUP;
+      groups[group][listener] = nullListener;
+    }
+    oldListeners.clear();
+  }
+  
+  static ListenerRet nullListener(ListenerArgs...) {
+    if constexpr (!std::is_void<ListenerRet>::value) {
+      return {};
+    }
+  }
+};
 
 #endif
