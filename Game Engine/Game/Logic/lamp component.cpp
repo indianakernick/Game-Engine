49ae1108738a6c4915aac6b24bc6bbbabade8e19
Kerndog73 <kerndog73@gmail.com>
2017-06-11 12:25:53 +0930

    Add Lamp tile

---
+++ Game Engine/Game/Logic/lamp component.cpp
@@ -0,0 +1,30 @@
+//
+//  lamp component.cpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 11/6/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include "lamp component.hpp"
+
+Game::Events::LampOn::LampOn(const Actor::ID id)
+  : lamp(id) {}
+
+Game::Events::LampOff::LampOff(const Actor::ID id)
+  : lamp(id) {}
+
+void Game::Lamp::update(uint64_t) {
+  setState(anyInputOn());
+}
+
+void Game::Lamp::setState(const bool newState) {
+  if (newState != state) {
+    if (newState) {
+      evtMan->emit<Events::LampOn>(actor->getID());
+    } else {
+      evtMan->emit<Events::LampOff>(actor->getID());
+    }
+    state = newState;
+  }
+}
---
+++ Game Engine/Game/Logic/lamp component.hpp
@@ -0,0 +1,54 @@
+//
+//  lamp component.hpp
+//  Game Engine
+//
+//  Created by Indi Kernick on 11/6/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef game_logic_lamp_component_hpp
+#define game_logic_lamp_component_hpp
+
+#include "tile component.hpp"
+#include "../../Engine/Event/manager.hpp"
+#include "../../Engine/Game Logic/actor.hpp"
+
+namespace Game {
+  namespace Events {
+    constexpr char LAMP_ON_NAME[] = "Lamp On";
+  
+    class LampOn final : public StaticEvent<LampOn, LAMP_ON_NAME> {
+    public:
+      explicit LampOn(Actor::ID);
+      ~LampOn() = default;
+      
+      const Actor::ID lamp;
+    };
+    
+    constexpr char LAMP_OFF_NAME[] = "Lamp Off";
+    
+    class LampOff final : public StaticEvent<LampOff, LAMP_OFF_NAME> {
+    public:
+      explicit LampOff(Actor::ID);
+      ~LampOff() = default;
+      
+      const Actor::ID lamp;
+    };
+  }
+
+  class Lamp final : public TileComponent {
+  public:
+    Lamp() = default;
+    ~Lamp() = default;
+    
+    void init(const tinyxml2::XMLElement *) override {}
+    void update(uint64_t) override;
+    
+  private:
+    bool state = false;
+    
+    void setState(bool);
+  };
+}
+
+#endif
