c48738bd09c39e2745049e1397ae092eb4787582
Kerndog73 <kerndog73@gmail.com>
2017-06-12 11:32:45 +0930

    Fix style of Utils::combine

--- Game Engine/Engine/Utils/combine.hpp
+++ Game Engine/Engine/Utils/combine.hpp
@@ -14,45 +14,45 @@
 #include "variadic type traits.hpp"
 
 namespace Utils {
-  template <typename FIRST>
-  constexpr uint_fit_t<FIRST> combine(FIRST first) {
-    return static_cast<uint_fit_t<FIRST>>(first);
+  template <typename First>
+  constexpr uint_fit_t<First> combine(const First first) {
+    return static_cast<uint_fit_t<First>>(first);
   }
 
-  template <typename FIRST, typename ...REST>
+  template <typename First, typename ...Rest>
   constexpr std::enable_if_t<
-    allEither<std::is_integral, std::is_enum, FIRST, REST...>,
-    uint_fit_t<FIRST, REST...>
+    allEither<std::is_integral, std::is_enum, First, Rest...>,
+    uint_fit_t<First, Rest...>
   >
-  combine(FIRST first, REST... rest) {
+  combine(const First first, const Rest... rest) {
     return (
-      static_cast<uint_fit_t<FIRST, REST...>>(first) << (
-        (sizeof(REST) + ...) * 8
+      static_cast<uint_fit_t<First, Rest...>>(first) << (
+        (sizeof(Rest) + ...) * 8
       )
     ) | combine(rest...);
   }
 
-  constexpr uint32_t boolCombine(bool first) {
+  constexpr uint32_t boolCombine(const bool first) {
     return static_cast<uint32_t>(first);
   }
 
-  template <typename ...REST>
-  constexpr std::enable_if_t<allSameAs<bool, REST...>, uint32_t>
-  boolCombine(bool first, REST... rest) {
-    return (static_cast<uint32_t>(first) << sizeof...(REST)) | boolCombine(rest...);
+  template <typename ...Rest>
+  constexpr std::enable_if_t<allSameAs<bool, Rest...>, uint32_t>
+  boolCombine(const bool first, const Rest... rest) {
+    return (static_cast<uint32_t>(first) << sizeof...(Rest)) | boolCombine(rest...);
   }
 
   //based on boost::hash_combine
 
-  template <typename FIRST>
-  size_t hashCombine(FIRST first) {
-    std::hash<FIRST> hasher;
+  template <typename First>
+  size_t hashCombine(First first) {
+    const std::hash<First> hasher;
     return hasher(first);
   }
 
-  template <typename FIRST, typename ...REST>
-  size_t hashCombine(FIRST first, REST... rest) {
-    std::hash<FIRST> hasher;
+  template <typename First, typename ...Rest>
+  size_t hashCombine(First first, Rest... rest) {
+    const std::hash<First> hasher;
     const size_t firstHash = hasher(first);
     return firstHash ^ (hashCombine(rest...) + 0x9e3779b9 + (firstHash << 6) + (firstHash >> 2));
   }
