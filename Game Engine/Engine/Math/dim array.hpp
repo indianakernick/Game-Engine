9473538aa537c85a6d9e013108351c31da639b5c
Kerndog73 <kerndog73@gmail.com>
2017-06-13 16:22:56 +0930

    Suppress warnings in Math::DimArray

--- Game Engine/Engine/Math/dim array.hpp
+++ Game Engine/Engine/Math/dim array.hpp
@@ -14,48 +14,64 @@
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 
+//The formulas are straight from wikipedia
+//https://en.wikipedia.org/wiki/Row-_and_column-major_order
+
+//GCC 7.1 and Clang 4.0 unroll the loops and inline the call to access
+
+//DimArray is just as efficient as manually writing out the formula
+
 namespace Math {
   template <typename Coord, size_t DIMENSIONS>
-  struct GetCoordsType {
+  struct CoordsTraits {
     using type = std::array<Coord, DIMENSIONS>;
+    
+    static Coord &access(type &coords, const size_t i) {
+      return coords[i];
+    }
+    
+    static const Coord &access(const type &coords, const size_t i) {
+      return coords[i];
+    }
   };
   
   template <typename Coord>
-  struct GetCoordsType<Coord, 2> {
+  struct CoordsTraits<Coord, 2> {
     using type = glm::tvec2<Coord>;
+    
+    static Coord &access(type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
+    
+    static const Coord &access(const type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
   };
   
   template <typename Coord>
-  struct GetCoordsType<Coord, 3> {
+  struct CoordsTraits<Coord, 3> {
     using type = glm::tvec3<Coord>;
+    
+    static Coord &access(type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
+    
+    static const Coord &access(const type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
   };
   
   template <typename Coord>
-  struct GetCoordsType<Coord, 4> {
+  struct CoordsTraits<Coord, 4> {
     using type = glm::tvec4<Coord>;
-  };
-  
-  template <typename Coords>
-  struct GetCoordsSize;
-  
-  template <typename Coord, size_t DIMENSIONS>
-  struct GetCoordsSize<std::array<Coord, DIMENSIONS>> {
-    static constexpr size_t value = DIMENSIONS;
-  };
-  
-  template <typename Coord>
-  struct GetCoordsSize<glm::tvec2<Coord>> {
-    static constexpr size_t value = 2;
-  };
-  
-  template <typename Coord>
-  struct GetCoordsSize<glm::tvec3<Coord>> {
-    static constexpr size_t value = 3;
-  };
-  
-  template <typename Coord>
-  struct GetCoordsSize<glm::tvec4<Coord>> {
-    static constexpr size_t value = 4;
+    
+    static Coord &access(type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
+    
+    static const Coord &access(const type &coords, const size_t i) {
+      return (&coords.x)[i];
+    }
   };
   
   enum class Order : uint8_t {
@@ -73,26 +89,25 @@ namespace Math {
     static_assert(std::is_integral<Coord>::value);
     static_assert(std::is_integral<Index>::value);
   
-    using Coords = typename GetCoordsType<Coord, DIMENSIONS>::type;
+    using Traits = CoordsTraits<Coord, DIMENSIONS>;
+    using Coords = typename Traits::type;
   
     DimArray() = default;
     explicit DimArray(const Coords newSize) {
-      for (size_t s = 0; s != GetCoordsSize<Coords>::value; s++) {
-        size[s] = newSize[s + 1];
-      }
+      setSize(newSize);
     }
     ~DimArray() = default;
   
     void setSize(const Coords newSize) {
-      for (size_t s = 0; s != GetCoordsSize<Coords>::value; s++) {
-        size[s] = newSize[s + 1];
+      for (size_t s = 0; s != DIMENSIONS; s++) {
+        size[s] = Traits::access(newSize, s + 1);
       }
     }
     
     Index posToIndex(const Coords pos) const {
       Index sum = 0;
       for (size_t p = 0; p != DIMENSIONS; p++) {
-        Index product = pos[p];
+        Index product = static_cast<Index>(Traits::access(pos, p));
         for (size_t s = p; s != DIMENSIONS - 1; s++) {
           product *= size[s];
         }
@@ -107,7 +122,7 @@ namespace Math {
       Coord product = 1;
       for (size_t p = DIMENSIONS - 2; p != -size_t(1); p--) {
         product *= size[p];
-        pos[p] = index / product;
+        Traits::access(pos, p) = static_cast<Coord>(index / product);
       }
       return pos;
     }
@@ -123,26 +138,25 @@ namespace Math {
     static_assert(std::is_integral<Coord>::value);
     static_assert(std::is_integral<Index>::value);
     
-    using Coords = GetCoordsType<Coord, DIMENSIONS>;
+    using Traits = CoordsTraits<Coord, DIMENSIONS>;
+    using Coords = typename Traits::type;
   
     DimArray() = default;
     explicit DimArray(const Coords newSize) {
-      for (size_t s = 0; s != GetCoordsSize<Coords>::value; s++) {
-        size[s] = newSize[s];
-      }
+      setSize(newSize);
     }
     ~DimArray() = default;
   
     void setSize(const Coords newSize) {
-      for (size_t s = 0; s != GetCoordsSize<Coords>::value; s++) {
-        size[s] = newSize[s];
+      for (size_t s = 0; s != DIMENSIONS; s++) {
+        size[s] = Traits::access(newSize, s);
       }
     }
     
     Index posToIndex(const Coords pos) const {
       Index sum = 0;
       for (size_t p = 0; p != DIMENSIONS; p++) {
-        Index product = pos[p];
+        Index product = static_cast<Index>(Traits::access(pos, p));
         for (size_t s = 0; s != p; s++) {
           product *= size[s];
         }
@@ -157,7 +171,7 @@ namespace Math {
       Coord product = 1;
       for (size_t p = 1; p != DIMENSIONS; p++) {
         product *= size[p - 1];
-        pos[p] = index / product;
+        Traits::access(pos, p) = static_cast<Coord>(index / product);
       }
       return pos;
     }
